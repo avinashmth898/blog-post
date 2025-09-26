e9ec1918a39271cd778975dc796937eaef98789b
Avinash Kumar <avinashmth898@gmail.com>
2025-09-27 01:06:54 +0530

    CROS updates

--- src/main/java/com/example/blog_viewer_api/config/SecurityConfig.java
+++ src/main/java/com/example/blog_viewer_api/config/SecurityConfig.java
@@ -2,28 +2,48 @@ package com.example.blog_viewer_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-    @Configuration
+
+import java.util.List;
+
+@Configuration
     public class SecurityConfig {
 
         // Create in-memory admin user
         @Bean
         public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
-            UserDetails admin = User.withUsername("admin")
-                    .password(passwordEncoder.encode("admin123")) // your password
+            UserDetails admin1 = User.withUsername("admin1")
+                    .password(passwordEncoder.encode("admin_1_123")) // your password
                     .roles("ADMIN")
                     .build();
-            return new InMemoryUserDetailsManager(admin);
+
+            UserDetails admin2 = User.withUsername("admin2")
+                    .password(passwordEncoder.encode("admin_2_123")) // your password
+                    .roles("ADMIN")
+                    .build();
+
+            UserDetails admin3 = User.withUsername("admin3")
+                    .password(passwordEncoder.encode("admin_3_123")) // your password
+                    .roles("ADMIN")
+                    .build();
+
+            return new InMemoryUserDetailsManager(admin1, admin2, admin3);
         }
+
 
         @Bean
         public PasswordEncoder passwordEncoder() {
@@ -34,15 +54,33 @@ import org.springframework.security.web.SecurityFilterChain;
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
             http
+
                     .csrf(AbstractHttpConfigurer::disable) // disable for simplicity (enable later with tokens)
                     .authorizeHttpRequests(auth -> auth
-                            .requestMatchers("/api/blogs","/api/blogs/*").permitAll() // default allow
-                            .requestMatchers("/api/blogs/**").hasRole("ADMIN") // override for write
+                            .requestMatchers(HttpMethod.GET, "/api/blogs/**").permitAll()
+                            .requestMatchers(HttpMethod.POST, "/api/blogs/**").hasRole("ADMIN")
+                            .requestMatchers(HttpMethod.PUT, "/api/blogs/**").hasRole("ADMIN")
+                            .requestMatchers(HttpMethod.DELETE, "/api/blogs/**").hasRole("ADMIN")
                             .anyRequest().authenticated()
                     )
-                .httpBasic(Customizer.withDefaults());
+
+                    .httpBasic(Customizer.withDefaults());
             // use basic auth (username+password in Postman)
 
             return http.build();
+        }
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+            CorsConfiguration configuration=new CorsConfiguration();
+            configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+            configuration.setAllowedHeaders(List.of("*"));
+            configuration.setAllowedMethods(List.of("GET","POST","PUT","OPTIONS","DELETE"));
+            configuration.setAllowCredentials(true);
+
+
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", configuration);
+
+            return source;
         }
     }
