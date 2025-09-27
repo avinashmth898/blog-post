6f6df1750dd360eb3d47780f79096abdfe50e932
Avinash Kumar <avinashmth898@gmail.com>
2025-09-27 17:39:34 +0530

    login checks fixed

--- src/main/java/com/example/blog_viewer_api/config/SecurityConfig.java
+++ src/main/java/com/example/blog_viewer_api/config/SecurityConfig.java
@@ -58,6 +58,7 @@ import java.util.List;
                     .csrf(AbstractHttpConfigurer::disable) // disable for simplicity (enable later with tokens)
                     .authorizeHttpRequests(auth -> auth
                             .requestMatchers(HttpMethod.GET, "/api/blogs/**").permitAll()
+                            .requestMatchers(HttpMethod.POST, "/api/blogs/login").permitAll()
                             .requestMatchers(HttpMethod.POST, "/api/blogs/**").hasRole("ADMIN")
                             .requestMatchers(HttpMethod.PUT, "/api/blogs/**").hasRole("ADMIN")
                             .requestMatchers(HttpMethod.DELETE, "/api/blogs/**").hasRole("ADMIN")
